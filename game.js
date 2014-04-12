fe5344fb673fc5b1264af24fec226e72d52fcea7
Dan <dan@dotdan.com>
2014-04-11 22:46:41 -0400

    Initial commit.

---
+++ game.js
@@ -0,0 +1,57 @@
+var Game = (function() {
+  var constructor = function() {
+    this.board = [
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0]
+    ];
+  };
+
+  constructor.prototype = {
+    get_board: function() {
+      return this.board;
+    },
+
+    find_empty_nodes: function() {
+      return _.flatten(this.board).reduce(function(memo, value, index) {
+        if(value === 0) {
+          memo.push(index);
+        }
+
+        return memo;
+      }, []);
+    },
+
+    select_random_empty_tile: function() {
+      var possible_nodes = this.find_empty_nodes();
+      var selected_node = Math.floor(Math.random() * possible_nodes.length);
+      return possible_nodes[selected_node];
+    },
+
+    get_tile_coords: function(index) {
+      var x = index % 5;
+      var y = ((index - x) / 5);
+      return { x: x, y: y };
+    },
+
+    set_random_tile: function(number) {
+      var coords = this.get_tile_coords(this.select_random_empty_tile());
+      this.board[coords.y][coords.x] = number;
+    }
+  };
+
+  return constructor;
+})();
+
+var game = new Game();
+
+// every game starts with a 2 and a 4
+var coords = game.get_tile_coords(game.select_random_empty_tile());
+game.board[coords.y][coords.x] = 2;
+
+var coords = game.get_tile_coords(game.select_random_empty_tile());
+game.board[coords.y][coords.x] = 4;
+
+console.log(JSON.stringify(game.board));
